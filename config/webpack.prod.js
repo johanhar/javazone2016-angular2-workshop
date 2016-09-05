d063012231b882e15e2ed75f5f2e324c258180da
Johannes Harestad <johannes.harestad@kantega.no>
2016-09-05 14:40:37 +0200

    Endret prod-config (bugs)

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,10 +4,8 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
-const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
-
 module.exports = webpackMerge(commonConfig, {
-  devtool: 'source-map',
+  devtool: 'cheap-module-eval-source-map',
 
   output: {
     path: helpers.root('dist'),
@@ -16,19 +14,7 @@ module.exports = webpackMerge(commonConfig, {
     chunkFilename: '[id].[hash].chunk.js'
   },
 
-  htmlLoader: {
-    minimize: false // workaround for ng2
-  },
-
   plugins: [
-    new webpack.NoErrorsPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
-    new ExtractTextPlugin('[name].[hash].css'),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'ENV': JSON.stringify(ENV)
-      }
-    })
+    new ExtractTextPlugin('[name].[hash].css')
   ]
 });
